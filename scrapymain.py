9acefb24892229836198819ddb2b648348710376
pengfeis <su.pengfei@live.com>
2015-09-07 00:13:24 +0800

    good version

--- scrapymain.py
+++ scrapymain.py
@@ -5,12 +5,18 @@ from bs4 import BeautifulSoup
 import pymysql
 import re
 import logging
-import os
 
 
 # Setting log
-logging.basicConfig(filename=os.path.join(os.getcwd(), 'log.txt'), level=logging.DEBUG, filemode='w', format='%(asctime)s - %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
+console = logging.StreamHandler()
+file_handler = logging.FileHandler('meta-data.log', encoding='utf-8')
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+console.setFormatter(formatter)
 
+logging.getLogger().addHandler(console)
+logging.getLogger().addHandler(file_handler)
 SCHOOL_CODE_PATTERN = "\[(\d{4})]"  # school code is 4 digit number
 
 cj = http.cookiejar.CookieJar()
@@ -55,19 +61,21 @@ event_validation = soup.find(id='__EVENTVALIDATION')['value']
 
 logging.info(view_state + '->' + event_validation)
 
-table = soup.find('table', id='yxlqqktj_table')
-table_rows = table.findAll('tr')
+html_table = soup.find('table', id='yxlqqktj_table')
+table_rows = html_table.findAll('tr')
+
+# Open a database connection
 conn = pymysql.connect(host='localhost', user='root', passwd='789632145', db='dev', port=3306, charset='utf8')
 cursor = conn.cursor()
 
 
-def get_next_page_data(view_state, event_validation):
+def get_next_page_data(next_page_view_state, next_page_event_validation):
     query_data2 = {
         '__EVENTTARGET': 'PagesUpDown$btnNext',
         '__EVENTARGUMENT': '',
-        '__VIEWSTATE': view_state,
+        '__VIEWSTATE': next_page_view_state,
         '__VIEWSTATEGENERATOR': '275FA2E0',
-        '__EVENTVALIDATION': event_validation,
+        '__EVENTVALIDATION': next_page_event_validation,
         'type': 'YXTJ',
         'button': '',
         'ddlNF_YXTJ': '2014',
@@ -80,12 +88,18 @@ def get_next_page_data(view_state, event_validation):
     request2 = urllib.request.Request(url, query_data2, header)
     data2 = urllib.request.urlopen(request2)
     html_data2 = data2.read().decode('gb2312')
-    return html_data2
+    next_page_soup = BeautifulSoup(html_data2, 'html.parser')
+    next_page_html_table = next_page_soup.find('table', id='yxlqqktj_table')
+    next_page_table_rows = next_page_html_table.findAll('tr')
+    extract_data(next_page_table_rows)
+    next_page_view_state = next_page_soup.find(id='__VIEWSTATE')['value']
+    next_page_event_validation = next_page_soup.find(id='__EVENTVALIDATION')['value']
+    return next_page_view_state, next_page_event_validation
 
 
-def extract_data(data):
+def extract_data(table_data):
     global tr, cols, school, matcher_code, school_code, school_name, kl, plan_num, act_num, top_score, min_score, avg_score, UPDATE_STATE
-    for tr in data:
+    for tr in table_data:
         cols = tr.findAll('td')
         if cols:
             school = cols[0].text
@@ -100,14 +114,20 @@ def extract_data(data):
             min_score = int(cols[5].text)
             avg_score = float(cols[6].text)
             UPDATE_STATE = 'UPDATE SCHOOL_SCORES SET 最低分 = %s, 录取最高分 = %s, 平均分 = %s, 录取人数 = %s WHERE 院校代号 = %s AND 院校名称 = %s AND 省份 = "河南" AND 科别 = %s AND 年份 = %s'
+            logging.info('%d,%d,%.1f,%d,%d,%s,%d,%d', min_score, top_score, avg_score, act_num, school_code, school_name, kl, 2014)
             cursor.execute(UPDATE_STATE, (min_score, top_score, avg_score, act_num, school_code, school_name, kl, 2014))
             conn.commit()
 
 
-next_p = get_next_page_data(view_state, event_validation)
+view_state, event_validation = get_next_page_data(view_state, event_validation)
+# extract_data(table_rows)
 
-extract_data(table_rows)
-print(next_p)
+
+while view_state and event_validation:
+    logging.info(view_state + '->' + event_validation)
+    view_state, event_validation = get_next_page_data(view_state, event_validation)
+
+
 
 
 
